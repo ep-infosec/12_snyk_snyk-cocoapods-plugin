a1b5411d8438f75afb67d9f46525292509c1df98
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-09 13:36:09 +0200

    init

---
+++ lib/index.ts
@@ -0,0 +1,206 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as crypto from 'crypto';
+import * as subProcess from './sub-process';
+import { LockfileParser } from '@snyk/cocoapods-lockfile-parser';
+import {
+  SingleSubprojectInspectOptions, SinglePackageResult,
+  PluginMetadata, SingleSubprojectPlugin,
+} from '@snyk/cli-interface/legacy/plugin';
+import { DepTree } from '@snyk/cli-interface/legacy/common';
+import { graphToDepTree } from '@snyk/dep-graph/dist/legacy';
+
+// Compile-time check that we are implementing the plugin API properly
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const _: SingleSubprojectPlugin = {
+  pluginName(): "snyk-cocoapods-plugin" {
+    return "snyk-cocoapods-plugin"
+  },
+  inspect,
+};
+
+const MANIFEST_FILE_NAMES = [
+  "CocoaPods.podfile.yaml",
+  "CocoaPods.podfile",
+  "Podfile",
+  "Podfile.rb",
+];
+
+const LOCKFILE_NAME = "Podfile.lock";
+
+export interface CocoaPodsInspectOptions extends SingleSubprojectInspectOptions {
+  strictOutOfSync?: boolean;
+}
+
+export async function inspect(
+  root: string,
+  targetFile?: string,
+  options?: CocoaPodsInspectOptions,
+): Promise<SinglePackageResult> {
+  if (!options) {
+    options = {dev: false};
+  }
+  if (!("strictOutOfSync" in options)) {
+    options.strictOutOfSync = false;
+  }
+
+  if (options.subProject) {
+    throw new Error("The CocoaPods plugin doesn't support specifying a subProject!");
+  }
+
+  let lockfilePath: string;
+  async function expectToFindLockfile(dir = '.'): Promise<string> {
+    const discoveredLockfilePath = await findLockfile(root, dir);
+    if (!discoveredLockfilePath) {
+      throw new Error("Could not find lockfile \"Podfile.lock\"! This might be resolved by running `pod install`.");
+    }
+    return discoveredLockfilePath;
+  }
+
+  let manifestFilePath: string | undefined;
+  if (targetFile) {
+    const { base, dir } = path.parse(targetFile);
+    if (base === LOCKFILE_NAME) {
+      lockfilePath = targetFile;
+      manifestFilePath = await findManifestFile(root, dir);
+    } else if (MANIFEST_FILE_NAMES.indexOf(base) !== -1) {
+      const absTargetFilePath = path.join(root, targetFile);
+      if (!await fsExists(absTargetFilePath)) {
+        throw new Error(`Given target file ("${targetFile}") doesn't exist!`)
+      }
+      manifestFilePath = targetFile;
+      lockfilePath = await expectToFindLockfile(dir);
+    } else {
+      throw new Error("Unexpected name for target file!");
+    }
+  } else {
+    manifestFilePath = await findManifestFile(root);
+    lockfilePath = await expectToFindLockfile();
+  }
+
+  const absLockfilePath = path.join(root, lockfilePath);
+
+  if (options.strictOutOfSync) {
+    if (!manifestFilePath) {
+      throw new Error("Option `--strict-out-of-sync=true` given, but no manifest file could be found!");
+    }
+    const absManifestFilePath = path.join(root, manifestFilePath);
+
+    const result = await verifyChecksum(absManifestFilePath, absLockfilePath);
+    if (result === ChecksumVerificationResult.NoChecksumInLockfile) {
+      throw new Error("Option `--strict-out-of-sync=true` given, but lockfile doesn't encode checksum of Podfile! "
+                    + "Try to update the CocoaPods integration via \"pod install\" or omit the option.");
+    }
+    if (result === ChecksumVerificationResult.Invalid) {
+      throw new OutOfSyncError(manifestFilePath, lockfilePath);
+    }
+  }
+
+  const plugin: PluginMetadata = {
+    meta: {},
+    name: 'cocoapods',
+    runtime: await cocoapodsVersion(root),
+    targetFile: manifestFilePath || lockfilePath,
+  };
+  const depTree = await getAllDeps(absLockfilePath);
+  return {
+    package: depTree,
+    plugin,
+  };
+}
+
+async function fsExists(pathToTest: string): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    try {
+      fs.exists(pathToTest, (exists) => resolve(exists))
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+
+async function fsReadFile(filename: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+        return
+      }
+      resolve(data);
+    })
+  });
+}
+
+async function findManifestFile(root: string, dir = '.'): Promise<string | undefined> {
+  for (const manifestFileName of MANIFEST_FILE_NAMES) {
+    const targetFilePath = path.join(root, dir, manifestFileName);
+    if (await fsExists(targetFilePath)) {
+      return path.join(dir, manifestFileName);
+    }
+  }
+}
+
+async function findLockfile(root: string, dir = '.'): Promise<string | undefined> {
+  const lockfilePath = path.join(root, dir, LOCKFILE_NAME);
+  if (await fsExists(lockfilePath)) {
+    return path.join(dir, LOCKFILE_NAME);
+  }
+}
+
+enum ChecksumVerificationResult {
+  Valid,
+  Invalid,
+  NoChecksumInLockfile
+}
+
+async function verifyChecksum(manifestFilePath: string, lockfilePath: string): Promise<ChecksumVerificationResult> {
+  const manifestFileContents = await fsReadFile(manifestFilePath);
+  const checksum = crypto.createHash('sha1').update(manifestFileContents).digest('hex');
+  const parser = await LockfileParser.readFile(lockfilePath);
+  if (parser.podfileChecksum === undefined) {
+    return ChecksumVerificationResult.NoChecksumInLockfile;
+  } else if (parser.podfileChecksum === checksum) {
+    return ChecksumVerificationResult.Valid;
+  } else {
+    return ChecksumVerificationResult.Invalid;
+  }
+}
+
+async function getAllDeps(lockfilePath: string): Promise<DepTree> {
+  let parser: LockfileParser;
+  try {
+    parser = await LockfileParser.readFile(lockfilePath);
+  } catch (error: any) {
+    throw new Error(`Error while parsing ${LOCKFILE_NAME}:\n${error.message}`);
+  }
+  const graph = parser.toDepGraph();
+  return graphToDepTree(graph, "cocoapods") as DepTree;
+}
+
+async function cocoapodsVersion(root: string): Promise<string> {
+  let podVersionOutput = '';
+  try {
+    // 1st: try to run CocoaPods via bundler
+    podVersionOutput = await subProcess.execute('bundle exec pod', ['--version'], {cwd: root});
+  } catch {
+    try {
+      // 2nd: try to run CocoaPods directly
+      podVersionOutput = await subProcess.execute('pod', ['--version'], {cwd: root});
+    } catch {
+      // intentionally empty
+    }
+  }
+  return podVersionOutput.trim();
+}
+
+export class OutOfSyncError extends Error {
+  public code = 422;
+  public name = 'OutOfSyncError';
+
+  public constructor(manifestFileName: string, lockfileName: string) {
+    super(`Your Podfile ("${manifestFileName}") is not in sync ` +
+      `with your lockfile ("${lockfileName}"). ` +
+      `Please run "pod install" and try again.`);
+    Error.captureStackTrace(this, OutOfSyncError);
+  }
+}
---
+++ test/lib/index.test.ts
@@ -0,0 +1,259 @@
+import { inspect } from '../../lib/index';
+import { fixtureDir } from '../common';
+import { execute } from '../../lib/sub-process';
+
+jest.mock('../../lib/sub-process');
+
+const mockedExecute = execute as unknown as jest.MockedFunction<typeof execute>;
+
+// Note: `any` is necessary due to a quirk between jest and TypeScript.
+// The propertyMatchers argument has the compile type `{ then: any, catch: any }`,
+// which is not the runtime type.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function singlePkgResultMatcher(regexp = /(^|\/)Podfile$/): any {
+  return {
+    plugin: {
+      targetFile: expect.stringMatching(regexp),
+    },
+  };
+}
+
+describe('inspect(rootDir, targetFile?, options?)', () => {
+  beforeEach(() => {
+    mockedExecute.mockResolvedValueOnce('1.7.5');
+  });
+
+  afterEach(() => {
+    mockedExecute.mockClear();
+  });
+
+  describe('includes the runtime version', () => {
+    test('from bundled CocoaPods', async () => {
+      const rootDir = fixtureDir('simple');
+
+      await expect(inspect(rootDir)).resolves.toMatchObject({
+        plugin: { runtime: '1.7.5' },
+      });
+
+      expect(mockedExecute.mock.calls).toEqual([
+        ['bundle exec pod', ['--version'], { cwd: rootDir }],
+      ]);
+    });
+
+    test('from globally installed CocoaPods if there is no bundled', async () => {
+      mockedExecute.mockReset();
+      mockedExecute.mockRejectedValueOnce(
+        new Error('Could not locate Gemfile or .bundle/ directory'),
+      );
+      mockedExecute.mockResolvedValueOnce('1.7.5');
+
+      const rootDir = fixtureDir('simple');
+
+      await expect(inspect(rootDir)).resolves.toMatchObject({
+        plugin: { runtime: '1.7.5' },
+      });
+
+      expect(mockedExecute.mock.calls).toEqual([
+        ['bundle exec pod', ['--version'], { cwd: rootDir }],
+        ['pod', ['--version'], { cwd: rootDir }],
+      ]);
+    });
+
+    test('but doesn’t fail if the CocoaPods CLI cannot be found', async () => {
+      mockedExecute.mockReset();
+      mockedExecute.mockRejectedValueOnce(
+        new Error('Could not locate Gemfile or .bundle/ directory'),
+      );
+      mockedExecute.mockRejectedValueOnce(new Error('command not found: pod'));
+
+      const rootDir = fixtureDir('simple');
+
+      await expect(inspect(rootDir)).resolves.toMatchObject({
+        plugin: { runtime: '' },
+      });
+
+      expect(mockedExecute.mock.calls).toEqual([
+        ['bundle exec pod', ['--version'], { cwd: rootDir }],
+        ['pod', ['--version'], { cwd: rootDir }],
+      ]);
+    });
+  });
+
+  describe('without targetFile argument', () => {
+    test('works with the simple fixture', async () => {
+      await expect(inspect(fixtureDir('simple'))).resolves.toMatchSnapshot(
+        singlePkgResultMatcher(),
+      );
+    });
+
+    test('works with the eigen fixture', async () => {
+      await expect(inspect(fixtureDir('eigen'))).resolves.toMatchSnapshot(
+        singlePkgResultMatcher(),
+      );
+    });
+
+    test('works with a fixture where no manifest file is present', async () => {
+      await expect(
+        inspect(fixtureDir('simple_without_manifest')),
+      ).resolves.toMatchSnapshot(singlePkgResultMatcher(/(^|\/)Podfile.lock$/));
+    });
+
+    test('works with a fixture using a legacy manifest file name', async () => {
+      await expect(
+        inspect(fixtureDir('legacy_manifest_file')),
+      ).resolves.toMatchSnapshot(singlePkgResultMatcher(/^CocoaPods.podfile$/));
+    });
+
+    test('fails with a fixture where no lockfile is present', async () => {
+      await expect(
+        inspect(fixtureDir('simple_without_lockfile')),
+      ).rejects.toThrow(
+        'Could not find lockfile "Podfile.lock"! This might be resolved by running `pod install`.',
+      );
+    });
+  });
+
+  describe('with a targetFile argument', () => {
+    describe('with the simple fixture', () => {
+      test('works with the Podfile', async () => {
+        await expect(
+          inspect(fixtureDir('simple'), 'Podfile'),
+        ).resolves.toMatchSnapshot(singlePkgResultMatcher());
+      });
+
+      test('works with the Podfile.lock', async () => {
+        await expect(
+          inspect(fixtureDir('simple'), 'Podfile.lock'),
+        ).resolves.toMatchSnapshot(singlePkgResultMatcher());
+      });
+
+      test('fails if the targetFile doesn’t exist', async () => {
+        await expect(
+          inspect(fixtureDir('simple'), './no/Podfile.lock'),
+        ).rejects.toMatchObject({
+          message: expect.stringMatching(
+            "ENOENT: no such file or directory, open '[^']*no/Podfile.lock'",
+          ),
+        });
+      });
+
+      test('fails if the targetFile uses an unsupported name', async () => {
+        await expect(
+          inspect(fixtureDir('simple'), 'Cacaofile'),
+        ).rejects.toMatchObject({
+          message: expect.stringMatching('Unexpected name for target file!'),
+        });
+      });
+    });
+
+    describe('with multi project fixture', () => {
+      test('works when the Podfile is in a subdir', async () => {
+        await expect(
+          inspect(fixtureDir('multi'), 'a/Podfile'),
+        ).resolves.toMatchObject(singlePkgResultMatcher(/^a\/Podfile$/));
+      });
+
+      test('works when the Podfile.lock is in a subdir', async () => {
+        await expect(
+          inspect(fixtureDir('multi'), 'a/Podfile.lock'),
+        ).resolves.toMatchObject(singlePkgResultMatcher(/^a\/Podfile$/));
+      });
+    });
+
+    describe('with a fixture where no manifest file is present', () => {
+      test('works with the Podfile.lock', async () => {
+        await expect(
+          inspect(fixtureDir('simple_without_manifest'), 'Podfile.lock'),
+        ).resolves.toMatchSnapshot(
+          singlePkgResultMatcher(/(^|\/)Podfile.lock$/),
+        );
+      });
+    });
+
+    describe('with a fixture where no lockfile is present', () => {
+      test('fails', async () => {
+        await expect(
+          inspect(fixtureDir('simple_without_lockfile'), 'Podfile'),
+        ).rejects.toThrow(
+          'Could not find lockfile "Podfile.lock"! This might be resolved by running `pod install`.',
+        );
+      });
+    });
+
+    describe('with a fixture using a legacy file name', () => {
+      test('works with the CocoaPods.podfile', async () => {
+        await expect(
+          inspect(fixtureDir('legacy_manifest_file')),
+        ).resolves.toMatchSnapshot(
+          singlePkgResultMatcher(/^CocoaPods.podfile$/),
+        );
+      });
+
+      test('fails if the targetFile doesn’t exist', async () => {
+        await expect(
+          inspect(fixtureDir('legacy_manifest_file'), 'Podfile'),
+        ).rejects.toThrow('Given target file ("Podfile") doesn\'t exist!');
+      });
+    });
+  });
+
+  describe('with options.strictOutOfSync=true as argument', () => {
+    test('succeeds when the checksum matches', async () => {
+      await expect(
+        inspect(fixtureDir('simple'), undefined, {
+          strictOutOfSync: true,
+        }),
+      ).resolves.not.toBeUndefined();
+    });
+
+    test('fails when the checksum mismatches', async () => {
+      await expect(
+        inspect(fixtureDir('simple_with_checksum_mismatch'), undefined, {
+          strictOutOfSync: true,
+        }),
+      ).rejects.toThrowError(
+        'Your Podfile ("Podfile") is not in sync with your lockfile ("Podfile.lock"). Please run "pod install" and try again.',
+      );
+    });
+
+    test('fails when there is no manifest', async () => {
+      await expect(
+        inspect(fixtureDir('simple_without_manifest'), undefined, {
+          strictOutOfSync: true,
+        }),
+      ).rejects.toThrow(
+        'Option `--strict-out-of-sync=true` given, but no manifest file could be found!',
+      );
+    });
+
+    test('fails when there is no checksum', async () => {
+      await expect(
+        inspect(fixtureDir('legacy_lockfile'), undefined, {
+          strictOutOfSync: true,
+        }),
+      ).rejects.toThrow(
+        'Option `--strict-out-of-sync=true` given, but lockfile doesn\'t encode checksum of Podfile! Try to update the CocoaPods integration via "pod install" or omit the option.',
+      );
+    });
+  });
+
+  test('fails if the targetFile cannot be parsed', async () => {
+    await expect(
+      inspect(fixtureDir('lockfile_in_conflict')),
+    ).rejects.toThrowError(
+      /^Error while parsing Podfile.lock:\ncan not read a block mapping entry/,
+    );
+  });
+
+  describe('with an options.subProject argument', () => {
+    test('fails', async () => {
+      await expect(
+        inspect(fixtureDir('simple'), undefined, {
+          subProject: 'doesntMatter',
+        }),
+      ).rejects.toThrow(
+        "The CocoaPods plugin doesn't support specifying a subProject!",
+      );
+    });
+  });
+});
